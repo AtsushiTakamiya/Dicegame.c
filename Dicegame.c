30704566d042f8f8b85c3717760a3d4f0fb79a95
s1260013 <s1260013@lsrdc7>
2019-07-09 16:46:53 +0900

    Add the program of greeting

---
+++ Dicegame.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  int Die1,Die2,T;
+  char Name[30];
+
+  printf("What is your name?\n");
+  scanf("%s",Name);
+  printf("Hello, %s!\n",Name);
+  
+  printf("Rolling the dice...\n");
+
+  Die1 = rand() % 6 + 1;
+  Die2 = rand() % 6 + 1;
+  T = Die1 + Die2;
+
+  printf("Die 1: %d\n",Die1);
+  printf("Die 2: %d\n",Die2);
+  printf("Total Value: %d\n",T);
+  
+  return 0;
+}

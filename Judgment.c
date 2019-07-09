8934ac73ef888808e89b9a5b61b7659f91cb92f2
s1260013 <s1260013@lsrdc7>
2019-07-09 16:31:45 +0900

    Add program of judgment

--- Judgment.c
+++ Judgment.c
@@ -3,13 +3,14 @@
 
 int main(){
   int T,Die1,Die2;
+  Char Name[30];
 
   T = Die1 + Die2;
 
   if(T > 7){
-    printf("You won\n");
+    printf("%s won!\n",Name);
   } else {
-    printf("You lost\n");
+    printf("%s lost!\n",Name);
   }
 
   return 0;

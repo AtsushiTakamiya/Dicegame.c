eb2b44239fa28c8952c82bc704b5632033e4510c
s1260013 <s1260013@lsrdc7>
2019-07-09 16:53:48 +0900

    Add changing of the program of greeting

--- Dicegame.c
+++ Dicegame.c
@@ -3,7 +3,12 @@
 
 int main(){
   int Die1,Die2,T;
+  char Name[30];
 
+  printf("What is your name?\n");
+  scanf("%s",Name);
+  printf("Hello, %s!\n",Name);
+  
   printf("Rolling the dice...\n");
 
   Die1 = rand() % 6 + 1;
@@ -13,6 +18,12 @@ int main(){
   printf("Die 1: %d\n",Die1);
   printf("Die 2: %d\n",Die2);
   printf("Total Value: %d\n",T);
+  
+  if(T > 7){
+    printf("%s won!\n",Name);
+  } else {
+    printf("%s lost!\n",Name);
+  }
   
   return 0;
 }
